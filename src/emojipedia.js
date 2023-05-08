8631421e438d0cd2ba7fe9635f37a460a639f309
Kim Bocua <abdulkarimbocua@gmail.com>
2023-05-08 15:16:58 +0800

    first commit

---
+++ src/emojipedia.js
@@ -0,0 +1,67 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🚀",
+    name: "Rocket",
+    meaning:
+      "A rocket is shot into space. “Come on, this is easy. It's not rocket science!“ Or like Buzz Lightyear of Toy Story said: To infinity and much farther."
+  },
+  {
+    id: 4,
+    emoji: "🥵",
+    name: "Overheated face",
+    meaning:
+      "It is so hot, I will get a heat stroke right away! Bright red head, the tongue is hanging from the mouth and drops of sweat are on the forehead. The hottest place in the world lies in Iran: In 2005, the highest temperature of 70.7 °C was measured in the Dasht-e Lut desert."
+  },
+  {
+    id: 5,
+    emoji: "🥳",
+    name: "Face with party blower and party hat",
+    meaning:
+      "“That was a wild party“ or “There's something to celebrate“. For pure party vibes, try the 🥳 emoji: a face in a party hat blowing a horn as confetti flies around its head. It symbolizes the joy of exuberant celebrations and happy occasions like New Year's Eve, birthdays, or World Emoji Day. This emoji expresses that there is something to celebrate or that you have had a wild party. Whatever the occasion, this emoji emphasizes the celebration. It shows that you are ready to enjoy an event to the fullest and look forward to what is to come. Additionally, it can stand for carnival and express the joy of the fifth season."
+  },
+  {
+    id: 6,
+    emoji: "💎",
+    name: "Gem Stone",
+    meaning:
+      "Diamonds are a girl's best friends. You are important to me and as precious as a diamond. Diamonds are for eternity. Symbol of luxury and wealth."
+  },  
+  {
+    id: 7,
+    emoji: "♟",
+    name: "Chess Piece",
+    meaning:
+      "That was a clever (chess) move, “checkmate“, or “treat someone like a chess piece“. Strategic board game for two people with 16 pieces each in light and dark color. Symbol of intelligence and power, but also the fight between good and evil."
+  },  
+  {
+    id: 8,
+    emoji: "👾",
+    name: "Alien Monster",
+    meaning:
+      "Let's play something! The monster smiley is known from Arcade and computer games. Is also often interpreted as an alien."
+  },
+  {
+    id: 9,
+    emoji: "🎮",
+    name: "Game Console",
+    meaning:
+      "Let's play! Invitation to friends to play together. A new video game is on the market and you will be busy in the near future. Be it fantasy or adventure game or car racing: console games are a popular pastime."
+  }
+];
+
+export default emojipedia;
